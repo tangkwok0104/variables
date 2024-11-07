a84841412f6f8418214c093e8b276717ac847d45
tangkwok0104 <kwoktang600@gmail.com>
2024-11-07 01:19:04 -0500

    updated

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,3 +46,7 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
